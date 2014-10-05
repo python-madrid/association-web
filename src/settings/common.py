3016453be270b848a6e1ac930265dca7d7de437e
Yamila Moreno <yamila.ms@gmail.com>
2014-10-05 23:59:32 +0200

    Add MEDIA settings to ckeditor uploads. (WIP)
    Not proud of this commit, just a WIP.

--- src/settings/common.py
+++ src/settings/common.py
@@ -87,8 +87,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_IMAGE_BACKEND = "pillow"
