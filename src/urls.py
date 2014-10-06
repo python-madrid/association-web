f1db2caa2580a22c20e16e4d0e2de8040185a041
Yamila Moreno <yamila.ms@gmail.com>
2014-10-06 11:56:53 +0200

    Fix static and media files

--- src/urls.py
+++ src/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -10,3 +12,5 @@ urlpatterns = patterns('',
     url(r'^', include('meetings.urls', namespace="meetings")),
     url(r'^ckeditor/', include('ckeditor.urls')),
 )
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
