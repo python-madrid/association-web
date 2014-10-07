9bd57b9e1c745e2f4e83bf99df02548638c9f6ec
Yamila Moreno <yamila.ms@gmail.com>
2014-10-07 21:21:42 +0200

    Move (web)/static into web/static dir.
    Also recover contrib.staticfiles

--- src/settings/common.py
+++ src/settings/common.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'web',
     'meetings',
     'ckeditor',
