507b2142e108348d35ac6ed0bc740f8759de8d3b
Yamila Moreno <yamila.ms@gmail.com>
2014-10-07 21:06:54 +0200

    Add redirect for old urls to new ones

--- src/web/urls.py
+++ src/web/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import HomeView
+from .views import HomeView, RedirectPostView
 
 
 urlpatterns = patterns('',
+    url(r'^post/(?P<slug>[\w\d\-]+)/$', RedirectPostView.as_view(), name='redirect-post'),
     url(r'^$', HomeView.as_view(), name='home'),
 )
--- src/web/views.py
+++ src/web/views.py
@@ -1,6 +1,8 @@
+from django.core.urlresolvers import reverse
 from django.views.generic.base import View
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 
 from meetings.models import Meeting
 
@@ -13,4 +15,8 @@ class HomeView(View):
             'web/home.html', context,
             context_instance=RequestContext(request)
         )
+
+class RedirectPostView(View):
+    def get(self, request, slug):
+        return HttpResponseRedirect(reverse('meetings:meeting', args=[slug]))
 
